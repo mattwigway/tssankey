0e89ddcab72e6b09ab03ef9a2707bf74ef1060a9
Matthew Wigginton Conway <matt@indicatrix.org>
2020-11-03 13:35:04 -0500

    black format code

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
+
 setup(
     name="tssankey",
     version="0.0.1",
     packages=find_packages(),
     scripts=[],
-
     # metadata to display on PyPI
     author="Matthew Wigginton Conway",
     author_email="matt@indicatrix.org",
--- tssankey/sankey.py
+++ tssankey/sankey.py
@@ -4,7 +4,9 @@ import matplotlib.patches
 
 # just using fill_between and offsetting curves vertically makes steep curves appear to get
 # thinner in the middle, because you perceive the width of the line perpendicular to its direction
-def cwcurve_poly (x_left, x_right, base_left, base_right, height, xy_ratio=1, steps=50, **kwargs):
+def cwcurve_poly(
+    x_left, x_right, base_left, base_right, height, xy_ratio=1, steps=50, **kwargs
+):
     # create the center of the curve
     # just define one, which we multiply as needed to get the right height
     # This is just the ys
@@ -15,19 +17,23 @@ def cwcurve_poly (x_left, x_right, base_left, base_right, height, xy_ratio=1, st
     smoothstep = 6 * smoothstepxs ** 5 - 15 * smoothstepxs ** 4 + 10 * smoothstepxs ** 3
     center_x -= np.min(center_x)
     center_x /= np.max(center_x)
-    center_x *= (x_right - x_left)
+    center_x *= x_right - x_left
     center_x += x_left
     center_y = smoothstep * (base_right - base_left) + base_left + height / 2
-    
+
     out_x = np.zeros(steps * 2)
     out_y = np.zeros(steps * 2)
-    xdiff = (center_x[1:] - center_x[:-1]) / xy_ratio # put in y units
+    xdiff = (center_x[1:] - center_x[:-1]) / xy_ratio  # put in y units
     ydiff = center_y[1:] - center_y[:-1]
-    ang = np.arctan2(ydiff, xdiff) + np.pi / 2 # rotate 90 degrees from direction of line
-    #print(np.degrees(ang))
-    for i, offset in enumerate([height / 2, - height / 2]):
-        #print(i, offset)
-        xoff = np.array([*(np.cos(ang) * offset), 0]) * xy_ratio # handle last offset due to fencepost problem
+    ang = (
+        np.arctan2(ydiff, xdiff) + np.pi / 2
+    )  # rotate 90 degrees from direction of line
+    # print(np.degrees(ang))
+    for i, offset in enumerate([height / 2, -height / 2]):
+        # print(i, offset)
+        xoff = (
+            np.array([*(np.cos(ang) * offset), 0]) * xy_ratio
+        )  # handle last offset due to fencepost problem
         yoff = np.array([*(np.sin(ang) * offset), offset])
         edge_x = center_x + xoff
         edge_y = center_y + yoff
@@ -35,17 +41,18 @@ def cwcurve_poly (x_left, x_right, base_left, base_right, height, xy_ratio=1, st
             # reverse this edge so polygon is wound
             edge_x = edge_x[::-1]
             edge_y = edge_y[::-1]
-            
-        out_x[i * steps:(i + 1) * steps] = edge_x
-        out_y[i * steps:(i + 1) * steps] = edge_y
-     
+
+        out_x[i * steps : (i + 1) * steps] = edge_x
+        out_y[i * steps : (i + 1) * steps] = edge_y
+
     xy = np.array([out_x, out_y]).T
-    #return xy, center_x, center_y
+    # return xy, center_x, center_y
     return matplotlib.patches.Polygon(xy, **kwargs)
+
 
 # just using fill_between and offsetting curves vertically makes steep curves appear to get
 # thinner in the middle, because you perceive the width of the line perpendicular to its direction
-def cwcurve (x_left, x_right, base_left, base_right, height, ax, steps=50, **kwargs):
+def cwcurve(x_left, x_right, base_left, base_right, height, ax, steps=50, **kwargs):
     # create the center of the curve
     # just define one, which we multiply as needed to get the right height
     # This is just the ys
@@ -56,10 +63,10 @@ def cwcurve (x_left, x_right, base_left, base_right, height, ax, steps=50, **kwa
     smoothstep = 6 * smoothstepxs ** 5 - 15 * smoothstepxs ** 4 + 10 * smoothstepxs ** 3
     center_x -= np.min(center_x)
     center_x /= np.max(center_x)
-    center_x *= (x_right - x_left)
+    center_x *= x_right - x_left
     center_x += x_left
     center_y = smoothstep * (base_right - base_left) + base_left + height / 2
-    
+
     # compute width: https://stackoverflow.com/questions/19394505
     fig = ax.get_figure()
     # width is float in [0, 1] to say how much of bbox is used by axis. 72 is inches to points
@@ -67,16 +74,27 @@ def cwcurve (x_left, x_right, base_left, base_right, height, ax, steps=50, **kwa
     axis_length_data = np.abs(ax.get_ylim()[1] - ax.get_ylim()[0])
     pts_per_data_unit = axis_length_pt / axis_length_data
     width = pts_per_data_unit * height
-    
-    xy = np.array([center_x, center_y]).T
-    return matplotlib.patches.Polygon(xy, closed=False, facecolor='none', lw=width, **kwargs)
 
-def tssankey (df, bar_width=0.4, figsize=(12, 8), total_gap=100, ax=None, weights=None, colors=None):
+    xy = np.array([center_x, center_y]).T
+    return matplotlib.patches.Polygon(
+        xy, closed=False, facecolor="none", lw=width, **kwargs
+    )
+
+
+def tssankey(
+    df,
+    bar_width=0.4,
+    figsize=(12, 8),
+    total_gap=100,
+    ax=None,
+    weights=None,
+    colors=None,
+):
     # make the sankey plot
     # this one is a little trickier because the bars are not the same in all of them
     if ax is None:
         f, ax = plt.subplots(figsize=figsize)
-    
+
     bases = {}
 
     cols = df.columns
@@ -91,29 +109,39 @@ def tssankey (df, bar_width=0.4, figsize=(12, 8), total_gap=100, ax=None, weight
                 hgt = (df[col] == val).sum()
 
             if colors is None:
-                color='C4' if i > 0 else None
+                color = "C4" if i > 0 else None
             else:
                 if val in colors:
                     color = colors[val]
                 else:
-                    color = 'C4'
+                    color = "C4"
 
-            rect = ax.bar([i], [hgt], bottom=base, width=bar_width, color=color, zorder=10)[0]
+            rect = ax.bar(
+                [i], [hgt], bottom=base, width=bar_width, color=color, zorder=10
+            )[0]
 
             # label it
             if weights is not None:
                 total = np.sum(weights)
             else:
                 total = len(df)
-            label = f'{val}\n({int(round(hgt / total * 100))}%)'
+            label = f"{val}\n({int(round(hgt / total * 100))}%)"
             if hgt / total < 0.05:
                 label = val
-            ax.annotate(label,
-                        xy=(rect.get_x() + rect.get_width() / 2, rect.get_y() + rect.get_height() / 2),
-                        xytext=(0, 0),  # 3 points vertical offset
-                        textcoords="offset points",
-                        ha='center', va='center', color='white',
-                        weight='bold', zorder=20)
+            ax.annotate(
+                label,
+                xy=(
+                    rect.get_x() + rect.get_width() / 2,
+                    rect.get_y() + rect.get_height() / 2,
+                ),
+                xytext=(0, 0),  # 3 points vertical offset
+                textcoords="offset points",
+                ha="center",
+                va="center",
+                color="white",
+                weight="bold",
+                zorder=20,
+            )
 
             bases[col][val] = base
             base += hgt + gap
@@ -126,21 +154,41 @@ def tssankey (df, bar_width=0.4, figsize=(12, 8), total_gap=100, ax=None, weight
         rbases = {k: v for k, v in bases[rcol].items()}
         for orig_idx, orig_val in enumerate(df[cols[0]].cat.categories):
             if colors is None or orig_val not in colors:
-                color = f'C{orig_idx}'
+                color = f"C{orig_idx}"
             else:
                 color = colors[orig_val]
 
             for lval in df[lcol].cat.categories:
                 for rval in df[rcol].cat.categories:
                     if weights is not None:
-                        count = np.sum(weights[(df[cols[0]] == orig_val) & (df[lcol] == lval) & (df[rcol] == rval)])
+                        count = np.sum(
+                            weights[
+                                (df[cols[0]] == orig_val)
+                                & (df[lcol] == lval)
+                                & (df[rcol] == rval)
+                            ]
+                        )
                     else:
-                        count = np.sum((df[cols[0]] == orig_val) & (df[lcol] == lval) & (df[rcol] == rval))
+                        count = np.sum(
+                            (df[cols[0]] == orig_val)
+                            & (df[lcol] == lval)
+                            & (df[rcol] == rval)
+                        )
                     if count == 0:
                         continue
-                        
-                    ax.add_patch(cwcurve(i + bar_width / 2, i + 1 - bar_width / 2, lbases[lval], rbases[rval], count,
-                                         ax=ax, edgecolor=color, alpha=0.25))
+
+                    ax.add_patch(
+                        cwcurve(
+                            i + bar_width / 2,
+                            i + 1 - bar_width / 2,
+                            lbases[lval],
+                            rbases[rval],
+                            count,
+                            ax=ax,
+                            edgecolor=color,
+                            alpha=0.25,
+                        )
+                    )
 
                     lbases[lval] += count
                     rbases[rval] += count
